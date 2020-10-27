331130dfc9b290256923c0db3cdbbf66f16ec23c
Alan Lu <alanlu1023@gmail.com>
2020-10-27 15:19:35 -0500

    Decrease target balance to 10k eth and add Ganache deterministic accounts

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ const supportedNodes = {
     dockerImage: 'trufflesuite/ganache-cli',
     containerWebsocketPort: 8545,
     runParams: [
+      '--deterministic',
       '--defaultBalanceEther', '1000000',
     ],
   }),
@@ -129,16 +130,25 @@ async function setupNode({ nodeType }) {
     }
   }
 
+  const newAccountKeys = [
+    '4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d',
+    '6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1',
+    '6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c',
+    '646f1ce2fdad0e6deeeb5c7e8e5543bdde65e86029e2fd9fc169899c440a7913',
+    'add53f9a7e588d003326d1cbf9e4a43c061aadd9bc938c843a79e7b4fd2ad743',
+    '395df67f0c2d2d9fe1ad08d1bc8b6627011959b79c53d7dd6a3536a33ab8a4fd',
+    'e485d098507f54e7733a205420dfddbe58db035fa577fc294ebd14db90767a52',
+    'a453611d9419d0e56f499079478fd72c37b251a94bfde4d19872c44cf65386e3',
+    '829e924fdf021ba3dbbc4225edfece9aca04b929d6e75613329ca6f1d31c0bb4',
+    'b0057716d5917badaf911b193b12b910811c1497b5bada8d7711f758981c3773',
+  ];
+  const prefixKeyWith0x = nodeType === 'ganache';
+  await Promise.all(newAccountKeys.map((key) => web3.eth.personal.importRawKey(
+    prefixKeyWith0x ? `0x${key}` : key,
+    '',
+  )));
+
   const accounts = await web3.eth.getAccounts();
-  const targetNumAccounts = 10;
-  if (accounts.length < targetNumAccounts) {
-    const numNewAccounts = targetNumAccounts - accounts.length;
-    accounts.push(...(
-      await Promise.all(Array.from(
-        { length: numNewAccounts },
-        () => web3.eth.personal.newAccount(''),
-      ))));
-  }
 
   if (nodeType !== 'nethermind') {
     const unlockTimeout = nodeType === 'openethereum' ? '0x0' : 0;
@@ -148,7 +158,7 @@ async function setupNode({ nodeType }) {
   const accountBalances = (await Promise.all(accounts.map(
     (account) => web3.eth.getBalance(account),
   ))).map(BigInt);
-  const targetBalance = BigInt(web3.utils.toWei('1000000'));
+  const targetBalance = BigInt(web3.utils.toWei('10000'));
 
   const [coinbase, ...otherAccounts] = accounts.map(
     (address, i) => ({ address, balance: accountBalances[i] }),
